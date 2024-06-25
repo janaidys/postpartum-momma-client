91ab160e42ba8d399a6c6fb7899949e879062ff9
Anaidys Jimenez <u.anaidys@gmail.com>
2024-06-25 17:14:00 -0400

    fixed create

--- ppm/src/components/Update.js
+++ ppm/src/components/Update.js
@@ -1,71 +1,103 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate, useLocation } from "react-router-dom";
 import blogData from "../data/posts";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faUpload} from "@fortawesome/free-solid-svg-icons";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUpload } from "@fortawesome/free-solid-svg-icons";
 
 const Update = ({}) => {
   const navigate = useNavigate();
-  const {postID} = useParams();
-  
+  const { postID } = useParams();
+
   const [post, setPost] = useState([]);
   useEffect(() => {
-    fetch(`http://localhost:3000/api/blog/edit/:id`, {
-  method: "GET",
-  'Content-Type':
-        'application/json'
-})
-  .then((response) => response.json())
-  .then((result) => setPost(result.data))
-  .catch((error)=> console.log(error))
-}, []);
+    fetch(`http://localhost:3000/api/blog/${postID}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        setPost(result.data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
 
-const handleUpdateSubmit = (event) => {
+  const handleUpdateSubmit = (event) => {
     event.preventDefault();
-    console.log("This method ran...")
+    console.log("This method ran...");
     const body = {
       banner: event.target.banner.value,
       title: event.target.title.value,
       blurb: event.target.blurb.value,
       article: event.target.article.value,
-      upload: event.target.upload.value
-      }
-      console.log(body);
-      fetch(`https://postpartum-momma.onrender.com/api/blog/${postID}`
-      , {
-        method: "PUT",
-        body: JSON.stringify(body)
+    };
+    console.log(body);
+    fetch(`http://localhost:3000/api/blog/edit/${postID}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+        navigate("/admin");
       })
-        .then((response) => response.json())
-        .then((result) => console.log(result), navigate("/admin"))
-        .catch((error)=> console.log(error))
-        console.log(postID)
-}
-return (
-      <div>
+      .catch((error) => console.log(error));
+    
+  };
+
+  return (
+    <div>
       <div className="container1">
         <h1 className="page-title">Update Blog Post</h1>
-        <form onSubmit = {handleUpdateSubmit} >
-        <div className="banner">
-            <input type={"file"} accept="image/*" id="banner-upload" style={{visibility:"hidden"}}/>
-            <label htmlFor="banner-upload" className="banner-upload-btn"><FontAwesomeIcon icon= {faUpload}/></label>
-        </div>
-        <br/>
-        <span className="blog">
-            <textarea type="text" className="title" placeholder="Blog title..."></textarea>
-  
-            <textarea type="text" className="blurb" placeholder="Write your home page blurb here..."></textarea>
-            <textarea type="text" className="article" placeholder="Start writing here..."></textarea>
-        </span>
+        <form onSubmit={handleUpdateSubmit}>
+          <div className="banner">
+            <input
+              name="banner"
+              type={"file"}
+              accept="image/*"
+              id="banner-upload"
+              style={{ visibility: "hidden" }}
+            />
+            <label htmlFor="banner-upload" className="banner-upload-btn">
+              <FontAwesomeIcon icon={faUpload} />
+            </label>
+          </div>
+          <br />
+          <span className="blog">
+            <textarea
+              name="title"
+              type="text"
+              className="title"
+              placeholder="Blog title..."
+            ></textarea>
 
-        <span className="blog-options">
-            <button className=" publish-btn">Publish</button>
-        </span>
+            <textarea
+              name="blurb"
+              type="text"
+              className="blurb"
+              placeholder="Write your home page blurb here..."
+            ></textarea>
+
+            <textarea
+              name="article"
+              type="text"
+              className="article"
+              placeholder="Start writing here..."
+            ></textarea>
+          </span>
+
+          <span className="blog-options">
+            <button className="publish-btn">Publish</button>
+          </span>
         </form>
+      </div>
     </div>
-    </div>
-    )
-  }
+  );
+};
 
 export default Update;
